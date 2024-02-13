5a833097aeab652f8c4d6faedcb9e3c2cdca0f56
juhoon212 <ws5501@naver.com>
2024-02-13 18:14:13 +0900

    Initial commit

---
+++ src/main/java/com/example/githubaction/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.githubaction.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @Value("${app.version}")
+    String version;
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello CICD! version: " + version;
+    }
+}
